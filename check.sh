31447142635a1b649cfb956f299951d79e8c751e
Matt Farina <matt@mattfarina.com>
2018-09-21 16:27:09 -0400

    /me grumbles and misspellings

--- check.sh
+++ check.sh
@@ -7,7 +7,7 @@ if [ ! -f /tmp/some-yaml-cache/some.yaml ]; then
 else
     # Stat commands here are for linux which is different from macOS (BSDs)
     d=$(diff /tmp/some-yaml-cache/some.yaml some.yaml)
-    if [[ ! -z $s ]]; then
+    if [[ ! -z $d ]]; then
         echo "New config is different from previous. Proceed as change"
     else
         echo "No changes detected."
