9f3efcdf57d08a56231b36753a17329dc33da706
Matt Farina <matt@mattfarina.com>
2018-09-21 16:04:10 -0400

    Final method?

--- check.sh
+++ check.sh
@@ -4,17 +4,14 @@ echo "Starting script..."
 
 changed=False
 
-if [ ! -f /tmp/some-yaml-cache/some.yaml.timestamp ]; then
-    echo "Timestamp file not found! Proceed as changed"
+if [ ! -f /tmp/some-yaml-cache/some.yaml ]; then
+    echo "Cached copy not found! Proceed as changed"
     changed=True
 else
     # Stat commands here are for linux which is different from macOS (BSDs)
-    t1=$(cat /tmp/some-yaml-cache/some.yaml.timestamp)
-    t2=$(stat --printf "%Y" some.yaml)
-    if [[ t1 != t2 ]]; then
-        echo $t1
-        echo $t2
-        echo "File timestamp is different. Proceed as change"
+    d=$(diff /tmp/some-yaml-cache/some.yaml some.yaml)
+    if [[ ! -z $s ]]; then
+        echo "New config is different from previous. Proceed as change"
         changed=True
     fi
 fi
@@ -25,4 +22,4 @@ fi
 
 ## update the cache
 mkdir -p /tmp/some-yaml-cache
-stat --printf "%Y" some.yaml > /tmp/some-yaml-cache/some.yaml.timestamp
+cp some.yaml /tmp/some-yaml-cache/some.yaml
