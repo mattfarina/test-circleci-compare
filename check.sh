12f2f6b4e835204ad0fb79f1bd21c40032d93d8c
Matt Farina <matt@mattfarina.com>
2018-09-21 12:00:44 -0400

    detect timestamp changes

--- check.sh
+++ check.sh
@@ -5,15 +5,19 @@ echo "Starting script..."
 if [ ! -f /tmp/some-yaml-cache/some.yaml ]; then
     echo "File not found!"
 else
+    # Check if file contents changed
     d=$(diff /tmp/some-yaml-cache/some.yaml some.yaml)
     if [[ ! -z "$d" ]]; then
         echo "File changed"
     fi
-fi
 
-# Stat commands here are for linux which is different from macOS (BSDs)
-stat --printf "%Y" /tmp/some-yaml-cache/some.yaml
-stat --printf "%Y" some.yaml
+    # Stat commands here are for linux which is different from macOS (BSDs)
+    t1=$(stat --printf "%Y" /tmp/some-yaml-cache/some.yaml)
+    t2=$(stat --printf "%Y" some.yaml)
+    if [[ t1 != t2 ]]; then
+        echo "File changed but content was same as reverted version"
+    fi
+fi
 
 mkdir -p /tmp/some-yaml-cache
 cp some.yaml /tmp/some-yaml-cache/some.yaml
