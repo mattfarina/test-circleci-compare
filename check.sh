ae082fd602fcaf7a940edc53b834e0d3994e278d
Matt Farina <matt@mattfarina.com>
2018-09-21 12:14:28 -0400

    New method based on timestamps

--- check.sh
+++ check.sh
@@ -2,22 +2,25 @@
 
 echo "Starting script..."
 
-if [ ! -f /tmp/some-yaml-cache/some.yaml ]; then
-    echo "File not found!"
-else
-    # Check if file contents changed
-    d=$(diff /tmp/some-yaml-cache/some.yaml some.yaml)
-    if [[ ! -z "$d" ]]; then
-        echo "File changed"
-    fi
+changed=False
 
+if [ ! -f /tmp/some-yaml-cache/some.yaml.timestamp ]; then
+    echo "Timestamp file not found! Proceed as changed"
+    changed=True
+else
     # Stat commands here are for linux which is different from macOS (BSDs)
-    t1=$(stat --printf "%Y" /tmp/some-yaml-cache/some.yaml)
+    t1=$(cat /tmp/some-yaml-cache/some.yaml.timestamp)
     t2=$(stat --printf "%Y" some.yaml)
     if [[ t1 != t2 ]]; then
-        echo "File changed but content was same as reverted version"
+        echo "File timestamp is different. Proceed as change"
+        changed=True
     fi
 fi
 
+if [[ changed == False ]]; then
+    echo "No changes detected."
+fi
+
+## update the cache
 mkdir -p /tmp/some-yaml-cache
-cp some.yaml /tmp/some-yaml-cache/some.yaml
+stat --printf "%Y" some.yaml > /tmp/some-yaml-cache/some.yaml.timestamp
